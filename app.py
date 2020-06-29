4dd996a719f80a3352723fd7cef1ecdedc98427d
gggxbbb <2331490629@qq.com>
2020-06-29 23:24:18 +0800

    finish 'io'

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask,request
+from flask import Flask,request,url_for,redirect
 
 import json
 from random import randint
+import os
 
 from config import name_length,pass_length
+
+if not os.path.isdir('notes'):
+    os.mkdir('notes')
 
 app = Flask(__name__)
 
@@ -21,14 +25,31 @@ def index():
     passwd = ''
     for _ in range(pass_length):
         passwd += pass_list[randint(0,len(pass_list) - 1)]
-    return name + '#' + passwd
+    return redirect(url_for('io',name = name) + '#' + passwd)
 
 
 # Basic I/O
-@app.route('/<string:name>', methods = ['GET','POST'])
+@app.route('/<string:name>.note', methods = ['GET','POST'])
 def io(name):
-    return name
-
+    note_new = False
+    if not os.path.isfile('notes/%s'%name):
+        with open('notes/%s'%name,'w') as f:
+            f.write(json.dumps({'md5':'','body':''}))
+            f.close()
+        note_new = True
+    if request.method == 'GET':
+        with open('notes/%s'%name,'r') as f:
+            body = f.read()
+            f.close()
+            return body
+    if request.method == 'POST':
+        with open('notes/%s'%name,'w') as f:
+            f.write(json.dumps({
+                'md5':request.form['md5'],
+                'body':request.form['body']
+                }))
+            f.close()
+            return {'msg':'OK'}
 
 # Debug
 if __name__ == '__main__':
